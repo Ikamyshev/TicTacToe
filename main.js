ee312f578973cbe2dbac922142e42577aad502d2
Igor Kamyshev <ikamyshev@gmail.com>
2018-08-26 16:19:23 +0300

    Minimal changes

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function canvasClicked(canvasNumber) {
     console.log("field numder")
 }
+
+function cellClicked(row, col) {
+    console.log("row numder", row)
+    console.log("col numder", col)
+    var img = document.getElementById('container').children[row].children[col].getElementsByClassName('field')
+    img.src = "img/o.svg"
+}
+
+// $('div').click(function(evt)
+// {
+//     // var currentId = this
+//     // currentId.sr
+// }
+// )
